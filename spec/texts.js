90ae5ade871b61bfb6de208058e69a7cf22c03e4
Cho, Yongjoon <w2000wkd@gmail.com>
2018-06-15 11:49:56 +0900

    Support non alphabet characters

--- spec/texts.js
+++ spec/texts.js
@@ -1,7 +1,7 @@
 'use babel'
 
 export const unsplittedParagraphs = `
-First paragraph. Second contains e.g. this and a.k.a. 1972. Is it third sentence? Fourth sentence! (We have parentheses. How do we handle that?)
+First paragraph. Second contains e.g. this and a.k.a. 1972. Is it third sentence? 네번째 문장! (We have parentheses. How do we handle that?)
 
 Second paragraph has an empty line above?!?!
   Third paragraph has no empty line above....
@@ -12,7 +12,7 @@ export const splittedParagraphs = `
 First paragraph.
 Second contains e.g. this and a.k.a. 1972.
 Is it third sentence?
-Fourth sentence!
+네번째 문장!
 (We have parentheses.
 How do we handle that?)
 
